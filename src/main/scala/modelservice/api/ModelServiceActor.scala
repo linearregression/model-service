9195e706c6d093a099a5f4db212ce54f5fe4ef9a
cdgore <cdgore@cdgore.com>
2015-10-01 16:35:27 -0700

    enable http server stats reporting

--- src/main/scala/modelservice/api/ModelServiceActor.scala
+++ src/main/scala/modelservice/api/ModelServiceActor.scala
@@ -1,15 +1,20 @@
 package modelservice.api
 
+import org.json4s._
+import org.json4s.jackson.Serialization
 import akka.actor.SupervisorStrategy.Restart
 import akka.actor._
 import akka.util.Timeout
+import akka.pattern.ask
+import spray.can.Http
+import spray.http.ContentTypes._
+import spray.http.HttpMethods._
+import spray.http.HttpHeaders._
+import spray.http._
+import scala.concurrent.duration._
 //import modelservice.core.ModelParser.ParseParametersAndStore
 import modelservice.core.{FeatureParser, ModelParser}
 import modelservice.storage.{ModelBroker, ModelStorage}
-import spray.can.Http
-import spray.http.HttpMethods._
-import spray.http._
-import scala.concurrent.duration._
 
 /**
  * REST actor to pass requests, initialize and maintain storage
@@ -28,6 +33,8 @@ class ModelServiceActor extends Actor with ActorLogging {
   }
 
   implicit val timeout: Timeout = 5.second
+  import context.dispatcher
+  implicit val formats = DefaultFormats
 
   def initModelStorage(): Unit = {
     modelStorage = Some(context watch(context actorOf(Props[ModelStorage], name="ModelStorage")))
@@ -43,6 +50,11 @@ class ModelServiceActor extends Actor with ActorLogging {
       log.info("attached")
     }
 
+    /**
+     * POST /predict
+     * Accepts a JSON representation of a set of features
+     * Returns predictions for the features and the given model
+     */
     case HttpRequest(POST, Uri.Path(putPath), _, entity, _) if putPath.startsWith("/predict") => {
       val splitPath = putPath.split("/")
       val modelKey = try {
@@ -61,17 +73,33 @@ class ModelServiceActor extends Actor with ActorLogging {
           val parseActor = context actorOf Props(new FeatureParser())
           parseActor ! ParseFeatures(entity, modelKey, paramKey, modelStorageActor, sender)
         }
-        case None => sender ! HttpResponse(entity = "Model storage not yet initialized")
+        case None => sender ! HttpResponse(
+          entity = "Model storage not yet initialized",
+          headers = List(Connection("close"))
+        )
       }
     }
 
+    /**
+     * POST /models
+     * Accepts a JSON representation of a model specification in the form of a feature manager
+     * Returns a key for the given model
+     */
     case HttpRequest(POST, Uri.Path("/models"), _, entity, _) =>
       val parseActor = context actorOf Props(new ModelParser())
       modelStorage match {
         case Some(modelStorageActor) => parseActor ! ParseModelAndStore(entity, None, modelStorageActor, sender)
-        case None => sender ! HttpResponse(entity="Model storage not yet initialized")
+        case None => sender ! HttpResponse(
+          entity = "Model storage not yet initialized",
+          headers = List(Connection("close"))
+        )
       }
 
+    /**
+     * POST /models/<model_key>
+     * Accepts a JSON representation of a model's parameters
+     * Returns a key for the given set of parameters
+     */
     case HttpRequest(PUT, Uri.Path(putPath), _, entity, _) if putPath.startsWith("/models") => {
       val modelKey = try {
         Some(putPath.split("/")(2))
@@ -93,20 +121,50 @@ class ModelServiceActor extends Actor with ActorLogging {
                 case None => parseActor ! ParseModelAndStore(entity, Some(key), modelStorageActor, sender)
               }
             }
-            case None => sender ! HttpResponse(entity = "Model storage not yet initialized")
+            case None => sender ! HttpResponse(
+              entity = "Model storage not yet initialized",
+              headers = List(Connection("close"))
+            )
           }
         }
         case None => {
-          sender ! HttpResponse(entity = s"Could not parse PUT request with path: $putPath")
+          sender ! HttpResponse(
+            entity = s"Could not parse PUT request with path: $putPath",
+            headers = List(Connection("close"))
+          )
         }
       }
     }
 
+    /**
+     * GET /models
+     * Returns a list of currently stored models
+     */
     case HttpRequest(GET, Uri.Path("/models"), _, entity, _) =>
       val modelBroker = ModelBroker.createActor(context)
       modelStorage match {
         case Some(modelStorageActor) => modelBroker ! GetAllKeysInStorage(modelStorageActor, sender)
-        case None => sender ! HttpResponse(entity="Model storage not yet initialized")
+        case None => sender ! HttpResponse(
+          entity = "Model storage not yet initialized",
+          headers = List(Connection("close"))
+        )
+      }
+
+    /**
+     * GET /server-stats
+     * Returns statistics for the HTTP server
+     */
+    case HttpRequest(GET, Uri.Path("/server-stats"), _, _, _) =>
+      val client = sender
+//      context.parent ? Http.GetStats onSuccess {
+      context.actorSelection("/user/IO-HTTP/listener-0") ? Http.GetStats onSuccess {
+        case x: spray.can.server.Stats => client ! HttpResponse(
+          entity = HttpEntity(
+            `application/json`,
+            Serialization.write(x)
+          ),
+          headers = List(Connection("close"))
+        )
       }
   }
 }
